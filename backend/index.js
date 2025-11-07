477fd337df93d1383ff8fc850e242d99c5e9ae18
mukulsingh24 <rmks1004@gmail.com>
2025-11-07 21:24:52 +0530

    mongo initialized

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,7 @@
 // node prerequisite
 const express = require('express')
 const cors =  require('cors')
-require('mong.env').config();
+require('dotenv').config({ path: './mong.env' });
 //security
 const fs = require('fs')
 const multer = require('multer')
