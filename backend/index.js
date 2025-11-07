fe6c107e7cc9672f951f9646dcf6d95a58511a7a
mukulsingh24 <rmks1004@gmail.com>
2025-11-07 19:28:28 +0530

    changes made in backend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,39 @@
+// node prerequisite
 const express = require('express')
-const CORS =  require('cors')
+const cors =  require('cors')
 
+//security
+const fs = require('fs')
+const multer = require('multer')
+
+// make sure the file gets uploaded
+const uploadDir = 'uploads/'
+if(!fs.existsSync(uploadDir)){
+    fs.mkdirSync(uploadDir)
+}
+
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb) => {
+        cb(null,uploadDir)
+    },
+    filename:(req,file,cb) =>{
+        cb(null,Date.now() + '-' + file.originalname)
+    }
+})
+
+const upload = multer({storage:storage})
+//server initialization
 const app = express();
-const port = 5000; 
+const port = 5000;
+app.use(cors())
 
-app.use(CORS())
+app.post('/upload' ,upload.single('file'),(req,res) =>{
+    res.status(200).send({message : 'File Uploaded'})
+})
 
+//server output
 app.listen(port,()=>{
     console.log(`Server Started on ${port}`)
 })
+
